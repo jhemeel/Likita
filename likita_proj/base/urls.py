8270cf5c9cfec2964a1c1f6759ed78011ca83054
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-09-11 02:02:44 +0100

    added static files routes

--- likita_proj/base/urls.py
+++ likita_proj/base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name="home" ),
@@ -13,3 +15,6 @@ urlpatterns = [
     
     path('contact-us', views.contact_us, name='contact-us'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

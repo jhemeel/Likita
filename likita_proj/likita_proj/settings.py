97574afe3540a6ee1e60e3eeb856d8d694af4059
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-09-27 10:42:10 +0100

    allowed host for dokto.com.ng

--- likita_proj/likita_proj/settings.py
+++ likita_proj/likita_proj/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY= env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1', 'www.dokto.com.ng']
 

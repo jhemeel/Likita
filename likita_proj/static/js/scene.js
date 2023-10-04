1301b12df382649001a429f4c50d301e2cead261
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-10-04 16:40:52 +0100

    some stylings adjusted

--- likita_proj/static/js/scene.js
+++ likita_proj/static/js/scene.js
@@ -1,7 +1,12 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+// import * as THREE from 'https://cdn.skypack.dev/three';
+
+import * as THREE from three
+
+console.log(typeof(THREE));
 
 
 const scene = new THREE.Scene()
+// console.log(scene)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5

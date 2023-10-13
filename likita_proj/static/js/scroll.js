9dfa85b2807ad145ae3cb33a3b8831f7a624a630
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-10-13 15:50:05 +0100

    blog page grid layout made

--- likita_proj/static/js/scroll.js
+++ likita_proj/static/js/scroll.js
@@ -1,13 +1,13 @@
 document.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
-      document.querySelector(".head-section").classList.add("active");
+      // document.querySelector(".head-section").classList.add("active");
       document.querySelector(".navbar").classList.add("active");
-      document.querySelector(".social-nav").classList.add("hide")
+      // document.querySelector(".social-nav").classList.add("hide")
 
     } else {
-      document.querySelector(".head-section").classList.remove("active");
+      // document.querySelector(".head-section").classList.remove("active");
       document.querySelector(".navbar").classList.remove("active");
-      document.querySelector(".social-nav").classList.remove("hide");
+      // document.querySelector(".social-nav").classList.remove("hide");
 
 
 
@@ -16,14 +16,14 @@ document.addEventListener("scroll", () => {
   
   document.addEventListener("load", () => {
     if (window.scrollY > 0) {
-      document.querySelector(".head-section").classList.add("active");
+      // document.querySelector(".head-section").classList.add("active");
       document.querySelector(".navbar").classList.add("active");
-      document.querySelector(".social-nav").classList.add("hide")
+      // document.querySelector(".social-nav").classList.add("hide")
 
     } else {
-      document.querySelector(".head-section").classList.remove("active");
+      // document.querySelector(".head-section").classList.remove("active");
       document.querySelector(".navbar").classList.remove("active");
-      document.querySelector(".social-nav").classList.remove("hide");
+      // document.querySelector(".social-nav").classList.remove("hide");
 
 
     }

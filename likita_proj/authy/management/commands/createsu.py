2a770be9842cf6a07eb8be112456d58862f087da
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-10-17 01:14:56 +0100

    automated createsuperuser

---
+++ likita_proj/authy/management/commands/createsu.py
@@ -0,0 +1,20 @@
+# authy/management/commands/createsu.py
+from django.conf import settings
+from base.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    username = settings.env("SUPER_USER_NAME")
+    password = settings.env("SUPER_USER_PASSWORD")
+    email = settings.env("SUPER_USER_EMAIL")
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username=self.username).exists():
+            User.objects.create_superuser(
+                username=self.username,
+                email = self.email,
+                password=self.password
+            )
+        print('Superuser has been created.')

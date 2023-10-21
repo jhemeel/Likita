b0e5a3d91cadfc3be12868ded4afffd37b8228c2
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-10-21 04:03:13 +0100

    db switches

--- likita_proj/likita_proj/settings.py
+++ likita_proj/likita_proj/settings.py
@@ -118,15 +118,15 @@ WSGI_APPLICATION = 'likita_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# database_url = env("DATABASE_URL")
-# DATABASES = {
-#      'default' :  dj_database_url.parse(
-#      database_url,
-#      conn_max_age=600,
-#      conn_health_checks=True,
+database_url = env("DATABASE_URL")
+DATABASES = {
+     'default' :  dj_database_url.parse(
+     database_url,
+     conn_max_age=600,
+     conn_health_checks=True,
    
-#  )
-#  }
+ )
+ }
 
 # DATABASES = {
 #     'default': {
@@ -140,15 +140,15 @@ WSGI_APPLICATION = 'likita_proj.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'dokto',
-       'USER': 'postgres',
-       'PASSWORD': 'Omolabake1',
-       'HOST': 'localhost',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'dokto',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Omolabake1',
+#        'HOST': 'localhost',
+#    }
+# }
 
 
 

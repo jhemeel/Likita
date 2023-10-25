e77a67b1feb326f122b42f2e6d7b03f5278f79de
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-10-25 02:49:04 +0100

    profile page styles edited new migrations

--- likita_proj/likita_proj/settings.py
+++ likita_proj/likita_proj/settings.py
@@ -20,6 +20,43 @@ SECRET_KEY = env(
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False) == True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
+
+
+
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(" ")
 

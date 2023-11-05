e95db6d39291b71124ad8c4a7788e145980d0b4f
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-11-05 21:35:47 +0100

    date_of_birth widget customized

--- likita_proj/profiles/forms.py
+++ likita_proj/profiles/forms.py
@@ -3,7 +3,11 @@ from base.models import User
 from .models import ReplyContact, ContactUs, Subscribe, SendNewsletter
 from django import forms
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class ProfileForm(ModelForm):
+    date_of_birth = forms.DateField(widget = DateInput)
     class Meta:
         model = User
         fields = ['avatar','bio','name','username','date_of_birth','gender','email', 'location','profession']        
